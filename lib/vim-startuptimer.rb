b6d37eb311d1d0b47cc9ee079000ce27f159010c
rhysd <lin90162@yahoo.co.jp>
2012-12-10 16:55:36 +0900

    add Runner class to execute vim

--- lib/vim-startuptimer.rb
+++ lib/vim-startuptimer.rb
@@ -1,5 +1,23 @@
 require "vim-startuptimer/version"
+require 'tempfile'
+require 'tmpdir'
 
-module VimStartuptimer
+module VimStartuptimer extend self
     # Your code goes here...
+
+    class Runner
+
+      def initialize(vim)
+        @dir = Dir::mktmpdir "vim-startuptimer-"
+        @vim = vim
+      end # def initialize
+
+      private
+
+      def run_vim
+        tmp = Tempfile.new "vim-startuptime", @dir
+        system("#{@vim} --startuptime #{tmp.path} -c quit")
+      end # def run_vim(dir)
+
+    end # class Runner
 end
